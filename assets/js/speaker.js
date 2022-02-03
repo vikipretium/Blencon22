6e5cfd4a0e41156a2684da526340f10490f62e3a
vikipretium <vighneshpasupathy@gmail.com>
2022-02-04 03:40:07 +0530

    fix desktop/mobile home page

--- assets/js/speaker.js
+++ assets/js/speaker.js
@@ -9,31 +9,31 @@ const speakerData = [
         name: 'Ian Hubert',
         topic: 'World building in Blender',
         bio: 'Ian Hubert has been making movies for over two decades now, and visual effects for nearly that long. He\'s been using Blender since 2004, with a specific focus on combining digital worlds with real life imagery.',
-        urlImage: './asssets/images/ian-hubert.jpg', 
+        urlImage: './assets/images/ian-hubert.jpg', 
     },
     {
         name: 'Dalai Felinto',
         topic: 'A backstage look at open source development',
         bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events. Worked as the development coordinator of Blender Code Quest 2018, and is one of the 2.8 x development coordinators.',
-        urlImage: './assets/images/df.jpg', 
+        urlImage: './assets/images/erindale.jpg', 
     },
     {
         name: 'Dalai Felinto',
         topic: 'A backstage look at open source development',
         bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events. Worked as the development coordinator of Blender Code Quest 2018, and is one of the 2.8 x development coordinators.',
-        urlImage: './assets/images/df.jpg', 
+        urlImage: './assets/images/daneil.jpg', 
     },
     {
         name: 'Dalai Felinto',
         topic: 'A backstage look at open source development',
         bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events. Worked as the development coordinator of Blender Code Quest 2018, and is one of the 2.8 x development coordinators.',
-        urlImage: './assets/images/df.jpg', 
+        urlImage: './assets/images/janvan.jpg', 
     },
     {
         name: 'Dalai Felinto',
         topic: 'A backstage look at open source development',
         bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events. Worked as the development coordinator of Blender Code Quest 2018, and is one of the 2.8 x development coordinators.',
-        urlImage: './assets/images/df.jpg',
+        urlImage: './assets/images/william_landgren.png',
     }
 ];
 
@@ -52,7 +52,6 @@ speakerData.forEach((speaker) => {
         <div class="speaker-details">
             <h3>${speaker.name}</h3>
             <p class="speaker-details-topic">${speaker.topic}</p>
-            <div class="speaker-details-line"></div>
             <hr class="speaker-details-line"></hr>
             <p class="speaker-details-bio">${speaker.bio}</p>
         </div>
