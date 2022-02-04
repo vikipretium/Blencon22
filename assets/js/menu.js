75e7fc62ae6a7f3689121772c6cc26722a94ffaf
vikipretium <vighneshpasupathy@gmail.com>
2022-02-04 14:21:08 +0530

    fix linters

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,36 +1,21 @@
-// operation menu and close 
+// operation menu and close
 const openMenu = document.querySelector('.ham-icon');
 const closeMenu = document.querySelector('.close-icon');
 
 const overlay = document.querySelector('.overlay');
 
 openMenu.addEventListener('click', () => {
-    overlay.style.display = 'block';
+  overlay.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', () => {
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
 });
-// operation menu and close 
 
-// operation  when links clicked in the menu
-// const overlayTabs = document.querySelector('.tabs');
 const newtabs = document.getElementsByClassName('tab');
-console.log(newtabs);
 
-// newtabs.forEach(element => {
-//     element.addEventListener('click', () => {
-//         overlay.style.display = 'none';
-//     });
-// });
-
-for(let i=0;i<newtabs.length;i++){
-    newtabs[i].addEventListener('click', () => {
-        overlay.style.display = 'none';
-    });
+for (let i = 0; i < newtabs.length; i += 1) {
+  newtabs[i].addEventListener('click', () => {
+    overlay.style.display = 'none';
+  });
 }
-
-// overlayTabs.addEventListener('click', () => {
-//     overlay.style.width = '0vw';
-// });
-
--- assets/js/speaker.js
+++ assets/js/speaker.js
@@ -1,49 +1,49 @@
 const speakerData = [
-    {
-        name: 'Dalai Felinto',
-        topic: 'A backstage look at open source development',
-        bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events.',
-        urlImage: './assets/images/df.jpg',
-    },
-    {
-        name: 'Ian Hubert',
-        topic: 'World building in Blender',
-        bio: 'Ian Hubert has been making movies for over two decades now, and visual effects for nearly that long. He\'s been using Blender since 2004, with a specific focus on combining digital worlds with real life imagery.',
-        urlImage: './assets/images/ian-hubert.jpg', 
-    },
-    {
-        name: 'Erindale Woodford',
-        topic: 'Creating Parametric Multiverse Madness',
-        bio: 'After turning away from a degree in Computer Animation and Visualization, Erin trained as a cabinet maker before completing an Interior Design degree and finding themselves back in the world of 3D',
-        urlImage: './assets/images/erindale.jpg', 
-    },
-    {
-        name: 'Daneil Bystedt',
-        topic: 'Creating blender demos, glowing tigers and tortured creatures',
-        bio: 'Daniel Bystedt is working as Head of modeling & Lead creature artists at Goodbye Kansas Studios. He has worked on such projects as The Walking Dead, Cyberpunk 2077 gametrailer',
-        urlImage: './assets/images/daneil.jpg', 
-    },
-    {
-        name: 'Jan van den Hemel',
-        topic: 'Consistency and small efforts in Blender',
-        bio: 'My goal is to help people create 3D art with the free open-source software Blender in the shortest, most efficient method possible. ',
-        urlImage: './assets/images/janvan.jpg', 
-    },
-    {
-        name: 'William Landgren',
-        topic: 'Creating Hollywood in Blender',
-        bio: '14 year old creating viral shorts using blender while doing school assignmets',
-        urlImage: './assets/images/william_landgren.png',
-    }
+  {
+    name: 'Dalai Felinto',
+    topic: 'A backstage look at open source development',
+    bio: 'Graduated in Architecture and Urban Studies, using Blender since 2003 and contributor to the code since 2.47. His participation in the Blender community includes a book, few papers, workshops and talks at blender events.',
+    urlImage: './assets/images/df.jpg',
+  },
+  {
+    name: 'Ian Hubert',
+    topic: 'World building in Blender',
+    bio: 'Ian Hubert has been making movies for over two decades now, and visual effects for nearly that long. He\'s been using Blender since 2004, with a specific focus on combining digital worlds with real life imagery.',
+    urlImage: './assets/images/ian-hubert.jpg',
+  },
+  {
+    name: 'Erindale Woodford',
+    topic: 'Creating Parametric Multiverse Madness',
+    bio: 'After turning away from a degree in Computer Animation and Visualization, Erin trained as a cabinet maker before completing an Interior Design degree and finding themselves back in the world of 3D',
+    urlImage: './assets/images/erindale.jpg',
+  },
+  {
+    name: 'Daneil Bystedt',
+    topic: 'Creating blender demos, glowing tigers and tortured creatures',
+    bio: 'Daniel Bystedt is working as Head of modeling & Lead creature artists at Goodbye Kansas Studios. He has worked on such projects as The Walking Dead, Cyberpunk 2077 gametrailer',
+    urlImage: './assets/images/daneil.jpg',
+  },
+  {
+    name: 'Jan van den Hemel',
+    topic: 'Consistency and small efforts in Blender',
+    bio: 'My goal is to help people create 3D art with the free open-source software Blender in the shortest, most efficient method possible. ',
+    urlImage: './assets/images/janvan.jpg',
+  },
+  {
+    name: 'William Landgren',
+    topic: 'Creating Hollywood in Blender',
+    bio: '14 year old creating viral shorts using blender while doing school assignmets',
+    urlImage: './assets/images/william_landgren.png',
+  },
 ];
 
 // selecting speaker div
-const speakerDiv = document.querySelector(".speakers-list");
+const speakerDiv = document.querySelector('.speakers-list');
 speakerData.forEach((speaker) => {
-    const speakerProject = document.createElement('div');
+  const speakerProject = document.createElement('div');
 
-    let speakerDetails = '';
-    speakerDetails = `
+  let speakerDetails = '';
+  speakerDetails = `
     <div class="speaker">
         <div class="speaker-img">
          <div class="checker"></div>
@@ -57,7 +57,6 @@ speakerData.forEach((speaker) => {
         </div>
     </div>`;
 
-speakerProject.innerHTML = speakerDetails;
-speakerDiv.appendChild(speakerProject);
-
+  speakerProject.innerHTML = speakerDetails;
+  speakerDiv.appendChild(speakerProject);
 });
